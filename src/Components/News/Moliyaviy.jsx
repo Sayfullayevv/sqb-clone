5a4a4acaeb83821a20552f92a9bac5b4ebdad494
Sayfullayev1407 <ageny61@gmail.com>
2025-10-20 11:43:22 +0500

    news-ad-finance

--- src/Components/News/Moliyaviy.jsx
+++ src/Components/News/Moliyaviy.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
 
 export default function Moliyaviy() {
   return (
-    <div>Moliyaviy</div>
-  )
+    <div id="moliyaviy">
+      <div className="moliyaviy-grid">
+        <div>
+          <p className="moliyaviy-date">18.10.2025</p>
+          <span>
+            O'zsanoatqurilishbank xalqaro kapital bozorida - ishonch va istiqbol
+            ramzi
+          </span>
+          <p>
+            Joriy yil oktyabr oyida O'zsanoatqurilishbank (SQB) London fond
+            birjasida qiymati 300 million AQSH doll...
+          </p>
+          <button>Batafsil</button>
+        </div>
+        <div>
+          <p className="moliyaviy-date">18.10.2025</p>
+          <span>
+            O'zsanoatqurilishbank xalqaro kapital bozorida - ishonch va istiqbol
+            ramzi
+          </span>
+          <p>
+            Joriy yil oktyabr oyida O'zsanoatqurilishbank (SQB) London fond
+            birjasida qiymati 300 million AQSH doll...
+          </p>
+          <button>Batafsil</button>
+        </div>
+        <div>
+          <p className="moliyaviy-date">18.10.2025</p>
+          <span>
+            O'zsanoatqurilishbank xalqaro kapital bozorida - ishonch va istiqbol
+            ramzi
+          </span>
+          <p>
+            Joriy yil oktyabr oyida O'zsanoatqurilishbank (SQB) London fond
+            birjasida qiymati 300 million AQSH doll...
+          </p>
+          <button>Batafsil</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
-import { Router , Link, BrowserRouter, Routes, Route } from 'react-router-dom'
-import Yangiliklar from './Yangiliklar'
-import Elonlar from './Elonlar'
-import Moliyaviy from './Moliyaviy'
-import Bozor from './Bozor'
-import './News.scss';
-
+import React from "react";
+import { Router, Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Yangiliklar from "./Yangiliklar";
+import Elonlar from "./Elonlar";
+import Moliyaviy from "./Moliyaviy";
+import Bozor from "./Bozor";
+import "./News.scss";
 
 export default function News() {
   return (
-    <section id='news'>
-        <div className="news">
-            <div className="news-selections">
-                <div className='yangiliklar'>
-                    <Link to={'/yangiliklar'}>Yangiliklar</Link>
-                </div>
-                <div className='elonlar'>
-                    <Link to={'/elonlar'}>E'lonlar</Link>
-                </div>
-                <div  className='moliyaviy'>
-                    <Link to={'/tahlil'}>Moliyaviy tahlil</Link>
-                </div>
-                <div className='izlanishlar'>
-                    <Link to={'/izlanishlar'}>
-                        Bozor tahlillari va izlanishlar
-                    </Link>
-                </div>
-            </div>
+    <section id="news">
+      <div className="news">
+        <div className="news-selections">
+          <div className="yangiliklar">
+            <Link to={"/"}>Yangiliklar</Link>
+          </div>
+          <div className="elonlar">
+            <Link to={"/elonlar"}>E'lonlar</Link>
+          </div>
+          <div className="moliyaviy">
+            <Link to={"/tahlil"}>Moliyaviy tahlil</Link>
+          </div>
+          <div className="izlanishlar">
+            <Link to={"/izlanishlar"}>Bozor tahlillari va izlanishlar</Link>
+          </div>
         </div>
+      </div>
+      <div className="news-section">
         <Routes>
-            <Route path='/yangiliklar' element={<Yangiliklar/>}/>
-            <Route path='/elonlar' element={<Elonlar/>}/>
-            <Route path='/tahlil' element={<Moliyaviy/>}/>
-            <Route path='/izlanishlar' element={<Bozor/>}/>
+          <Route path="/" element={<Yangiliklar />} />
+          <Route path="/elonlar" element={<Elonlar />} />
+          <Route path="/tahlil" element={<Moliyaviy />} />
+          <Route path="/izlanishlar" element={<Bozor />} />
         </Routes>
+      </div>
     </section>
-  )
+  );
 }
